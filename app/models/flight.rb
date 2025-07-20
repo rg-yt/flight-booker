9defca67a36d83edcb33fa9482a76bd4b533866c
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 13:51:47 -0400

    Added a method to create flight labels
    Added a method to create flight labels for
    creating radio buttons.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
-  belongs_to :departure_airport, class_name: 'Airport'
-  belongs_to :arrival_airport, class_name: 'Airport'
+  belongs_to :departure_airport, class_name: "Airport"
+  belongs_to :arrival_airport, class_name: "Airport"
+
+  def flight_label
+    "#{departure_airport.code} to #{arrival_airport.code} on #{datetime.strftime("%B %d %Y at %I:%M %P")}"
+  end
 end
