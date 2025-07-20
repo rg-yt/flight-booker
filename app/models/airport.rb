0772e03e15ee9cdec8632f25d3289d12a6f6ec65
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 15:58:37 -0400

    Update model relations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,5 @@
 class Airport < ApplicationRecord
   has_many :arriving_flights, foreign_key: "arrival_airport_id", class_name: "Flight"
   has_many :departing_flights, foreign_key: "departure_airport_id", class_name: "Flight"
-  has_many :bookings, thorugh: :flights
+  has_many :bookings, through: :flights
 end
