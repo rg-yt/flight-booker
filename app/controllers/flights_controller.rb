0b815af6cec72d6e07923af0cbd80fbcc0e9f8ce
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 14:10:43 -0400

    Update the num passenger in view and controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,6 @@
 class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |airport| [ airport.code, airport.id ] }
-    @passenger_options = (1..4)
     @dates = Flight.pluck(:datetime).sort.uniq
     @date_options = @dates.map { |datetime| [ datetime.strftime("%B %d %Y"), datetime ] }.uniq
 
