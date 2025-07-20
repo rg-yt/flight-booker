33550cd2948be18dac26e489c7a22d6cf072247e
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 14:54:50 -0400

    Update airport and flight model associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+  has_many :bookings
 
   def flight_label
     "#{departure_airport.code} to #{arrival_airport.code} on #{datetime.strftime("%B %d %Y at %I:%M %P")}"
