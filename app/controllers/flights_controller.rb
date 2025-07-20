7fc6e84eb1482fbd3b0634c016db4b9a7c9150e9
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-19 23:50:21 -0400

    Add search logic to flight controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,9 @@ class FlightsController < ApplicationController
     @passenger_options = (1..4)
     @dates = Flight.pluck(:datetime).sort.uniq
     @date_options = @dates.map { |datetime| [ datetime.strftime("%B %d %Y"), datetime ] }.uniq
+
+    if params
+      @flights = Flight.where(departure_airport_id: params[:outbound_airport], arrival_airport_id: params[:destination_airport])
+    end
   end
 end
