5624021f72c08493924d5a5687536dfbe1598e6a
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 22:51:54 -0400

    Create new and create actions on bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,22 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    params[:passengers].to_i.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking, notice: "You've succesfully booked your flight"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.expect(booking: [ :flight_id, passengers_attributes: [ [ :name, :email ] ] ])
   end
 end
