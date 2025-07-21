323d123ae1a03cf2120e1fdb8854576d2fb2ecb4
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-20 22:51:13 -0400

    Create update migrations to change associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_20_182800) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_21_004819) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
@@ -19,11 +19,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_20_182800) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "flight_id", null: false
-    t.integer "passenger_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -41,8 +39,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_20_182800) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "booking_id"
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
-  add_foreign_key "bookings", "passengers"
+  add_foreign_key "passengers", "bookings"
 end
